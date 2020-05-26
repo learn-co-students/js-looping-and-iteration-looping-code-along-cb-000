d15a72645021b162fcffce340f8d973e5fc231c4
s-blais <stephen@s-blais.com>
2020-05-26 13:25:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  const collectedMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    collectedMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return collectedMessages;
+}
+
+/* function countDown(num) {
+  let i = num;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+} */
+
+// let's remove the redundant use of "i":
+
+function countDown(num) {
+  while (num >=0) {
+    console.log(num);
+    num--;
+  }
+}
